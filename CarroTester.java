cc0ab5e62fa060296bd462bf31a60f8547db74c1
Natalia Cabral Andrade <nataliia.andrade@hotmail.com>
2020-03-04 14:43:31 -0300

    Create CarroTester.java

---
+++ CarroTester.java
@@ -0,0 +1,32 @@
+package br.lasalle.oop.trab1;
+
+public class MotoTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Carro c1;
+		c1 = new Carro();
+		c1.modelo = "HB20";
+		c1.marca = "Hyundai";
+		c1.cor = "Branco";
+		c1.placa = 5502;
+		c1.ligado = true;
+    c2.parado();
+    c2.movimento();
+		
+
+	
+        
+       Carro c2 = new Carro();
+       	c2.modelo = "Palio";
+       	c2.marca = "Fiat";
+       	c2.cor = "Vermelho";
+       	c2.placa = 4948;
+       	c2.ligada = false;
+       	c2.parado();
+        c2.movimento();
+
+	}
+
+}
